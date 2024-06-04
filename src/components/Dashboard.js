341adbcf68e83d8fa91aeb625fb094ace2d2775e
Ridho Gymnastiar Al Rasyid <ridhoga186@gmail.com>
2024-06-05 00:45:46 +0700

    Initial Commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,24 @@
 // src/components/Dashboard.js
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {jwtDecode} from "jwt-decode";
+import {useNavigate} from "react-router-dom";
 
 const Dashboard = () => {
     const token = localStorage.getItem('token');
-    const {name} = jwtDecode(token);
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/login');
+        } else {
+            const {name} = jwtDecode(token);
+            setUsername(name)
+        }
+    }, [token, navigate]);
     return (
         <div>
-            <h2>Welcome, {name}</h2>
+            <h2>Welcome, {username}</h2>
         </div>
     );
 };
